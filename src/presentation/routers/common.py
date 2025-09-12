23773ebacffbbbac552eed2afc2663809d7a600b
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-09-13 01:52:52 +0400

    Update common.py

--- src/presentation/routers/common.py
+++ src/presentation/routers/common.py
@@ -1,16 +1,26 @@
 from aiogram import Router
 
-from src.presentation.routers.post.start import router as post_start_router
-from src.presentation.routers.post.dialog import dialog as post_dialog_router
+from src.presentation.routers.menu.start import router as menu_start_router
+from src.presentation.routers.menu.dialog import dialog as menu_dialog_router
 
 from src.presentation.routers.registration.start import router as registration_start_router
 from src.presentation.routers.registration.dialog import dialog as registration_dialog_router
 
+from src.presentation.routers.createpost.start import router as create_post_start_router
+from src.presentation.routers.createpost.dialog import dialog as create_post_dialog_router
+
+from src.presentation.routers.myposts.start import router as my_posts_start_router
+from src.presentation.routers.myposts.dialog import dialog as my_posts_dialog_router
+
 common_router = Router()
 
 common_router.include_routers(
-    post_start_router,
-    post_dialog_router,
+    menu_start_router,
+    menu_dialog_router,
     registration_start_router,
     registration_dialog_router,
+    create_post_start_router,
+    create_post_dialog_router,
+    my_posts_start_router,
+    my_posts_dialog_router,
 )
