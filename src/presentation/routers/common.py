3fe697863de306c932fe891cc502c671b9e38052
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-09-15 02:11:20 +0400

    Update common.py

--- src/presentation/routers/common.py
+++ src/presentation/routers/common.py
@@ -12,15 +12,20 @@ from src.presentation.routers.createpost.dialog import dialog as create_post_dia
 from src.presentation.routers.myposts.start import router as my_posts_start_router
 from src.presentation.routers.myposts.dialog import dialog as my_posts_dialog_router
 
+from src.presentation.routers.admin.start import router as admin_start_router
+from src.presentation.routers.admin.dialog import dialog as admin_dialog_router
+
 common_router = Router()
 
 common_router.include_routers(
-    menu_start_router,
-    menu_dialog_router,
     registration_start_router,
-    registration_dialog_router,
+    menu_start_router,
     create_post_start_router,
-    create_post_dialog_router,
     my_posts_start_router,
+    admin_start_router,
+    registration_dialog_router,
+    menu_dialog_router,
+    create_post_dialog_router,
     my_posts_dialog_router,
+    admin_dialog_router,
 )
