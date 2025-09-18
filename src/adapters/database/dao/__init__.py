14cc45b4eddc588868ece7832710ed6cf83375ed
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-09-19 02:32:02 +0400

    Update __init__.py

--- src/adapters/database/dao/__init__.py
+++ src/adapters/database/dao/__init__.py
@@ -1,3 +1,4 @@
 from .post import AbstractPostDAO, PostDAO
 from .common import AbstractCommonDAO, CommonDAO
 from .user import AbstractUserDAO, UserDAO
+from .price import AbstractPriceDAO, PriceDAO
